02871639654004151dcc3c26c863a5907071aa8b
siripatnaik <31252897+siripatnaik@users.noreply.github.com>
2017-09-11 20:57:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,8 +64,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter',function (req, res) {
+var counter = 0;
+app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
