4ee669a2c9b3412bf6e8cea7436d9d6be5b88a6a
siripatnaik <31252897+siripatnaik@users.noreply.github.com>
2017-09-11 20:54:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.get('/', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter',function(req,res) {
-    counter=counter+1;
+app.get('/counter',function (req, res) {
+    counter = counter + 1;
     res.send(counter.toString());
 });
 
